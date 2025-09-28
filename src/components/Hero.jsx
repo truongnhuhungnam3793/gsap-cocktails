3f7fa5c0ad3d1106a537c9b1be3be2c6ecb43fec
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-09-28 21:48:58 +0700

    Add Hero component to App and include placeholder div

---
+++ src/components/Hero.jsx
@@ -0,0 +1,93 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { SplitText } from "gsap/all"
+
+const Hero = () => {
+  useGSAP(() => {
+    const heroSplitText = new SplitText(".title", {
+      type: "chars, words",
+    })
+
+    const paragraphSplitText = new SplitText(".subtitle", {
+      type: "lines",
+    })
+
+    heroSplitText.chars.forEach((char) => char.classList.add("text-gradient"))
+
+    gsap.from(heroSplitText.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    })
+
+    gsap.from(paragraphSplitText.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+      delay: 1,
+    })
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(
+        ".left-leaf",
+        {
+          y: -200,
+        },
+        0
+      )
+      .to(
+        ".right-leaf",
+        {
+          y: 200,
+        },
+        0
+      )
+  }, [])
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Refreshing.</p>
+              <p className="subtitle">Sip the Spirit of Summer</p>
+            </div>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail is made with fresh ingredients and a passion for
+                perfecting every pour, whether you're celebrating or simply
+                relaxing.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+export default Hero
